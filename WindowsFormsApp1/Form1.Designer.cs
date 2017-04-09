56834d8404ae703e8da7fad17fe1a68a788c8728
swapnil1104 <swapniltiwari775@gmail.com>
2017-04-09 14:30:01 +0530

    Character seperation working.

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,9 +47,9 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(93, 100);
+            this.pictureBox1.Location = new System.Drawing.Point(93, 32);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.Size = new System.Drawing.Size(100, 118);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
@@ -62,11 +63,19 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 241);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
@@ -74,6 +83,7 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,6 +92,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using AForge.Imaging.Filters;
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
@@ -21,33 +21,53 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Bitmap bmp = new Bitmap("E:\\ocr\\h.jpg");
+            Bitmap bmp = new Bitmap("E:\\ocr\\h2.jpg");
             bmp.SetResolution(90, 60);
             pictureBox1.Image = bmp;
+
 
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Bitmap bmp = new Bitmap("E:\\ocr\\h.jpg");
+            Bitmap bmp = new Bitmap("E:\\ocr\\h2.jpg");
             bmp.SetResolution(90, 60);
-            int i = 0,x=0,p=0;
+            int rgb;
+            Color c;
+
+            int i = 0, x = 0, p = 0;
             int[] xaxis = new int[90];
-            for(x=1;x<60;x+=2)
+            Grayscale filter = new Grayscale(0.2125, 0.7154, 0.0721);
+            Bitmap grayimage = filter.Apply(bmp);
+
+            Threshold th = new Threshold();
+            th.ApplyInPlace(grayimage);          
+
+          
+            pictureBox1.Image = grayimage;
+       
+            for(x = 1; x < 60; x++)
             {
                 int f = 0;
-                for(int y =1;y<90;y++)
+                for (int y = 1; y < 90; y++)
                 {
-                    if (bmp.GetPixel(x, y).Equals(Color.Black)) f++;
-
+                    if (grayimage.GetPixel(x, y).Name == "ff000000") { 
+                        f = 1; }
+                    
                 }
                 if (f == 0)
                 {
                     xaxis[p++] = x;
                 }
             }
-            
+            for (p = 0; p < 90; p++)
+            {
+                textBox1.Text += xaxis[p] + " ";
+            }
+          
         }
+        
     }
+   
 }
