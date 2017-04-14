5fc61007be1b04d7536fa520dc35abcff94aa098
swapnil1104 <swapniltiwari775@gmail.com>
2017-04-14 14:26:41 +0530

    Final Prototype For Image Segmentation.

--- WindowsFormsApp1/Form3.Designer.cs
+++ WindowsFormsApp1/Form3.Designer.cs
@@ -31,23 +31,32 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(0, 1);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(282, 134);
+            this.pictureBox1.Size = new System.Drawing.Size(647, 115);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 170);
+            this.button1.Location = new System.Drawing.Point(12, 293);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(314, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Load";
             this.button1.UseVisualStyleBackColor = true;
@@ -55,35 +64,76 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(197, 170);
+            this.button2.Location = new System.Drawing.Point(322, 293);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(313, 23);
             this.button2.TabIndex = 2;
             this.button2.Text = "Process";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox1
+            // pictureBox2
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 229);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(260, 20);
-            this.textBox1.TabIndex = 3;
+            this.pictureBox2.Location = new System.Drawing.Point(81, 153);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 90);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(184, 153);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(60, 90);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(286, 153);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(60, 90);
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(378, 153);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(60, 90);
+            this.pictureBox5.TabIndex = 7;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(474, 153);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(60, 90);
+            this.pictureBox6.TabIndex = 8;
+            this.pictureBox6.TabStop = false;
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(647, 515);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form3";
             this.Text = "Form3";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -92,6 +142,10 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
     }
 }
--- WindowsFormsApp1/Form3.cs
+++ WindowsFormsApp1/Form3.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,27 +21,26 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Bitmap bmp = new Bitmap("E://ocr//ocr.png");
+            Bitmap bmp = new Bitmap("E://ocr//ocr.jpg");
             pictureBox1.Image = bmp;
         
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Bitmap bmp = new Bitmap("E://ocr//ocr.png");
+            Bitmap bmp = new Bitmap("E://ocr//ocr.jpg");
            
 
             int xmax = bmp.Width;
             int ymax = bmp.Height;
             Bitmap bwimage = processImage(bmp);
-          
+            int counter = 0;
             pictureBox1.Image = bwimage;
-
             int[] xaxis = new int[xmax];
             int[] yaxis = new int[ymax];
             int[] tempx = new int[xmax];
             int[] tempy = new int[ymax];
-            int y, x, i = 2 ;
+            int y, x, i = 1,j = 1;
             int iy = 1;
             int ix = 1;
             yaxis[0] = 0;
@@ -60,14 +60,11 @@ namespace WindowsFormsApp1
                
                 if ((tempy[p-1] != tempy[p] - 1) || (tempy[p] + 1 != tempy[p + 1]))
                 {
-                    yaxis[i] = tempy[p];
-                    i++;
+                    yaxis[i++] = tempy[p]+1;
+                   
                 }
             }
-            for(i = 2; i<ymax; i++)
-            {
-                //Console.WriteLine(yaxis[i]);
-            }
+          
 
             for (x = 1; x <xmax; x++)
             {
@@ -80,25 +77,45 @@ namespace WindowsFormsApp1
                     }
                 }
             }
-            xaxis[1] = tempx[1] + 1;
-            i = 2;
-            for (int p = 1; p < xmax; p++)
+            i = 1;
+             for (int p = 1; p < xmax; p++)
             {
 
                 if ((tempx[p - 1] != tempx[p] - 1) || (tempx[p] + 1 != tempx[p + 1]))
                 {
-                    xaxis[i] = tempx[p];
+                    xaxis[i] = tempx[p] +1;
                     i++;
                 }
             }
             for (i = 0; i < ymax; i++)
             {
-                Console.WriteLine(yaxis[i]);
+                Console.WriteLine(yaxis[i]+ " " + xaxis[i]);
             }
-            Console.WriteLine(xaxis[2]+" "+ yaxis[2] + " " + (xaxis[3] - xaxis[2] )+ " " + (yaxis[3] - yaxis[2]));
-            Crop filter = new Crop(new Rectangle(xaxis[2],yaxis[2],xaxis[3]-xaxis[2],yaxis[3]-yaxis[2]));
-            Bitmap croppedImg = filter.Apply(bwimage);
-            pictureBox1.Image = croppedImg;
+            //  Console.WriteLine(xaxis[2]+" "+ yaxis[2] + " " + (xaxis[3] - xaxis[2] )+ " " + (yaxis[3] - yaxis[2]));
+
+
+            int k;
+            Bitmap[] croppedImg = new Bitmap[30];
+            Bitmap temp;
+
+
+            for (i = 1, j = 2, k = 0; k <5 ;k++)
+            {
+                
+                Console.WriteLine(xaxis[i] + " " + yaxis[1] + " " + (xaxis[j] - xaxis[i]) + " " + (yaxis[2] - yaxis[1]));
+              
+                          
+                
+                Crop filter = new Crop(new Rectangle(xaxis[i], yaxis[1], (xaxis[j] - xaxis[i]), yaxis[2] - yaxis[1]));
+                croppedImg[k]= filter.Apply(bwimage);
+                i = i + 2;
+                j = j + 2;
+            }
+            pictureBox2.Image = croppedImg[0];
+            pictureBox3.Image = croppedImg[1];
+            pictureBox4.Image = croppedImg[2];
+            pictureBox5.Image = croppedImg[3];
+            pictureBox6.Image = croppedImg[4];
 
 
 
